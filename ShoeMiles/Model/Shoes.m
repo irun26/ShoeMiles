6d6bca1f7828d57b2c8628460c18eedc163670e3
Jimson Vedua <jimved@Jimsons-MacBook-Pro.local>
2016-08-06 22:02:42 -0400

    first run

---
+++ ShoeMiles/Model/Shoes.m
@@ -0,0 +1,13 @@
+//
+//  Shoes.m
+//  ShoeMiles
+//
+//  Created by Jimson Vedua on 8/6/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import "Shoes.h"
+
+@implementation Shoes
+
+@end
