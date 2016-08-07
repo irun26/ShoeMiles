045acc769b2dfdf62e04fd19a1f5869c3285a31e
Jimson Vedua <jimved@Jimsons-MacBook-Pro.local>
2016-08-06 20:59:42 -0400

    initial commit storyboard

---
+++ ShoeMiles/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  ShoeMiles
+//
+//  Created by Jimson Vedua on 8/6/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *milesRan;
+@property (weak, nonatomic) IBOutlet UITextField *shoesWorn;
+@property (weak, nonatomic) IBOutlet UITextField *dateOfRun;
+@property (weak, nonatomic) IBOutlet UIButton *submitButton;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
